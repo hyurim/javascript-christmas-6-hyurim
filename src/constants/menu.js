4e5b6bbeba8682d80ee6487722d22fc0e6b2eec9
Hyurim <kyo2301@naver.com>
2023-11-09 21:23:43 +0900

    feat(menu.js): 메뉴 변수화

---
+++ src/constants/menu.js
@@ -0,0 +1,23 @@
+const APPETIZER = Object.freeze({
+  soup: { menu: "양송이수프", price: 6000 },
+  tappas: { menu: "타파스", price: 5500 },
+  caesar_salad: { menu: "시저샐러드", price: 8000 },
+});
+
+const MAIN = Object.freeze({
+  t_bone: { menu: "티본스테이크", price: 55000 },
+  barbecue: { menu: "바비큐립", price: 54000 },
+  seafood: { menu: "해산물파스타", price: 35000 },
+  christmas: { menu: "크리스마스파스타", price: 25000 },
+});
+
+const DESSERT = Object.freeze({
+  chocolate_cake: { menu: "초코케이트", price: 15000 },
+  ice_cream: { menu: "아이스크림", price: 5000 },
+});
+
+const DRINK = Object.freeze({
+  zero_cola: { menu: "제로콜라", price: 3000 },
+  red_wine: { menu: "레드와인", price: 60000 },
+  champagne: { menu: "샴페인", price: 25000 },
+});
