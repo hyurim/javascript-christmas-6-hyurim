5149bff02acc81231f1a40dd03c0f16d14bc8238
Hyurim <kyo2301@naver.com>
2023-11-12 20:14:06 +0900

    refactor(preDiscountAmount.js): 옵셔널 체이닝 사용

--- src/domain/preDiscountAmount.js
+++ src/domain/preDiscountAmount.js
@@ -5,22 +5,16 @@ const { zero } = NUMBERS;
 
 const preDiscountAmount = (menuNames, quantities) => {
   const orderedFoods = menuNames.map((menu, index) => {
-    const menuItem = MENU.find((food) => food.menu === menu);
-    const quantity =
-      quantities && quantities[index] !== null
-        ? parseInt(quantities[index], 10)
-        : zero;
-    const price = menuItem ? menuItem.price : zero;
-    return {
-      menu,
-      quantity,
-      price,
-    };
+    const menuItem = MENU.find((food) => food.menu === menu) || { price: zero };
+    const quantity = parseInt(quantities?.[index], 10) || zero;
+    const price = menuItem.price;
+    return { menu, quantity, price };
   });
 
   const totalAmount = orderedFoods.reduce((total, food) => {
     return total + food.price * food.quantity;
   }, zero);
+
   return totalAmount;
 };
 
