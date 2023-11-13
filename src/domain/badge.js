a4d221836e6254a8e5582dde8c0fd5095e0785f1
Hyurim <kyo2301@naver.com>
2023-11-13 15:06:09 +0900

    docs(badge.js): badge 객체 JSDos 추가

--- src/domain/badge.js
+++ src/domain/badge.js
@@ -2,9 +2,8 @@ import { BADGE } from "../constants/prompt.js";
 import OutputView from "../OutputView.js";
 
 /**
- * 
- * @param {number} totalDiscountPrice - 총 할인 금액
- * @returns {Object} 
+ * 총 할인 금액에 해당하는 뱃지를 찾아서 출력하는 객체
+ * @param {number} totalDiscountPrice - 총 할인 금액 (음수)
  */
 const badge = (totalDiscountPrice) => {
   const selectedBadge = BADGE.find(
