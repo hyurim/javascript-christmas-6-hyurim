4fba98eabb47dfc221cd7613e1c9890d954a5bd0
Hyurim <kyo2301@naver.com>
2023-11-13 15:00:07 +0900

    fix(BadgeTest.js): badge.js 수정으로 인한 테스트 코드 수정

--- __tests__/domain/BadgeTest.js
+++ __tests__/domain/BadgeTest.js
@@ -1,40 +1,76 @@
+import { Console } from "@woowacourse/mission-utils";
 import badge from "../../src/domain/badge.js";
 
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(Console, "print");
+  logSpy.mockClear();
+
+  return logSpy;
+};
+
 describe("badge 기능 테스트", () => {
-  test("테스트 케이스 1: 할인 금액이 0일 때 undefined가 반환되이어야 함.", () => {
+  let logSpy;
+
+  beforeEach(() => {
+    logSpy = getLogSpy();
+  });
+
+  test("테스트 케이스 1: 할인 금액이 0일 때 없음이 출력되어야 함.", () => {
+    // given
     const totalDiscountPrice = 0;
 
-    const result = badge(totalDiscountPrice);
+    // when
+    badge(totalDiscountPrice);
 
-    expect(result).toEqual(undefined);
+    // then
+    const expected = "없음"
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(expected));
   });
 
-  test("테스트 케이스 2: 할인 금액이 2만원이 넘을 때 산타 배지가 반환되어야 함", () => {
+  test("테스트 케이스 2: 할인 금액이 2만원이 넘을 때 산타가 출력되어야 함", () => {
+    // given
     const totalDiscountPrice = -20001;
 
-    const result = badge(totalDiscountPrice);
+    // when
+    badge(totalDiscountPrice);
 
-    expect(result.badge).toEqual("산타");
+    // then
+    const expected = "산타"
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(expected));
   });
 
-  test("테스트 케이스 2: 할인 금액이 1만원 이상 2만원 미만일 때 트리 배지가 반환되어야 함", () => {
+  test("테스트 케이스 3: 할인 금액이 1만원 이상 2만원 미만일 때 트리가 출력되어야 함", () => {
+    // given
     const totalDiscountPrice = -19999;
 
-    const result = badge(totalDiscountPrice);
+    // when
+    badge(totalDiscountPrice);
 
-    expect(result.badge).toEqual("트리");
+    // then
+    const expected = "트리"
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(expected));
   });
-  test("테스트 케이스 3: 할인 금액이 5천원 이상 1만원 미만일 때 별 배지가 반환되어야 함.", () => {
+  test("테스트 케이스 4: 할인 금액이 5천원 이상 1만원 미만일 때 별이 출력되어야 함.", () => {
+    // given
     const totalDiscountPrice = -9999;
 
-    const result = badge(totalDiscountPrice);
+    // when
+    badge(totalDiscountPrice);
 
-    expect(result.badge).toEqual("별");
+    // then
+    const expected = "별"
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(expected));
   });
-  test("테스트 케이스 4: 할인 금액이 0원 이상일 때 undefined가 반환되이어야 함.", () => {
+  test("테스트 케이스 5: 할인 금액이 0원 이상일 때 없음이 출력되이어야 함.", () => {
+    // given
     const totalDiscountPrice = 1000;
 
-    const result = badge(totalDiscountPrice);
-    expect(result).toEqual(undefined);
+    // when
+    badge(totalDiscountPrice);
+
+    // then
+    const expected = "없음"
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(expected));
   });
 });
--- src/domain/badge.js
+++ src/domain/badge.js
@@ -1,12 +1,16 @@
 import { BADGE } from "../constants/prompt.js";
 import OutputView from "../OutputView.js";
 
+/**
+ * 
+ * @param {number} totalDiscountPrice - 총 할인 금액
+ * @returns {Object} 
+ */
 const badge = (totalDiscountPrice) => {
   const selectedBadge = BADGE.find(
     (select) => -totalDiscountPrice >= select.limit
   );
   OutputView.badge(selectedBadge);
-  return selectedBadge;
 };
 
 export default badge;
