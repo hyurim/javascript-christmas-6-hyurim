601067af94b49815b653a12bdc8e343abbda9822
Hyurim <kyo2301@naver.com>
2023-11-13 17:06:49 +0900

    docs(InputError.js): JSDos 추가

--- src/errors/InputError.js
+++ src/errors/InputError.js
@@ -1,5 +1,11 @@
 import messageFormat from "../constants/messageFormat.js";
 
+/**
+ * 사용자 입력과 관련된 오류를 나타내는 클래스입니다.
+ * @class
+ * @extends {Error}
+ * @param {string} message - 오류 메시지
+ */
 class InputError extends Error {
   constructor(message) {
     super(messageFormat.errorMessage(message));
