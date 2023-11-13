ba936a3e8df61c94189a5d53b35f45ea64c5632f
Hyurim <kyo2301@naver.com>
2023-11-13 21:13:30 +0900

    fix(App.js): JSDos 수정

--- src/App.js
+++ src/App.js
@@ -7,29 +7,28 @@ import ChristmasController from "./controller/ChristmasController.js";
 class App {
   constructor() {
     /**
-     * 크리스마스 컨트롤러 인스턴스
+     * ChristmasController 인스턴스
      * @type {ChristmasController}
      */
     this.christmasController = new ChristmasController();
   }
   /**
-   * 애플리케이션 실행 메서드
+   * 앱 실행
    * @async
-   * @returns {Promise<void>} Promise 객체
+   * @function
+   * @returns {Promise<void>} - 애플리케이션 실행 결과
    */
   async run() {
     /**
-     * 입력받은 날짜
+     * 날짜 입력 결과
      * @type {string}
      */
     const date = await this.christmasController.dateInput();
     /**
-     * 입력받은 음식과 개수
+     * 음식 입력 결과
      * @type {string}
      */
     const food = await this.christmasController.foodInput();
-
-    // 크리스마스 로직 처리
     this.christmasController.handleChristmasLogic(date, food);
   }
 }
--- src/utils/dateIsValid.js
+++ src/utils/dateIsValid.js
@@ -1,11 +1,24 @@
 import ERROR from "../constants/error.js";
 import InputError from "../errors/InputError.js";
 
+/**
+ * 입력된 날짜가 유효한지 검사함.
+ * 
+ * @param {string} date - 날짜 정보
+ * @returns {boolean} - 날짜가 유효하면 true, 유효하지 않을 경우 예외 발생
+ * @throws {InputError} - 날짜가 유효하지 않은 경우 발생하는 예외
+ */
 const dateIsValid = (date) => {
   validTest(date);
   return true;
 };
 
+/**
+ * 날짜 형식이 맞는지 검사.
+ * 
+ * @param {string} date - 날짜 정보
+ * @throws {InputError} - 날짜 형식이 맞지 않는 경우 발생하는 예외
+ */
 const validTest = (date) => { 
   const regex = /^([1-9]|[12][0-9]|3[0-1])$/
   if (!regex.test(date)) {
