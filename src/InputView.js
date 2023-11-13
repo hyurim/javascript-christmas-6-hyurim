503f3b64f55680247d1f6601521bcad5fd9370f2
Hyurim <kyo2301@naver.com>
2023-11-13 14:37:03 +0900

    docs(ChristmasController.js): 클래스 JSDoc 주석 추가

--- src/InputView.js
+++ src/InputView.js
@@ -3,10 +3,24 @@ import { PROMPT } from "./constants/prompt.js";
 
 const { select_date, select_menu } = PROMPT;
 
+/**
+ * 사용자로부터 입력을 받는 객체
+ */
 const InputView = {
+  /**
+   * 사용자로부터 날짜 입력을 받는 메서드
+   * @async
+   * @returns {Promise<string>} // 입력받은 날짜
+   */
   async date() {
     return await Console.readLineAsync(select_date);
   },
+
+  /**
+   * 사용자로부터 음식 주문을 받는 메서드
+   * @async
+   * @returns {Promise<string>} 입력받은 음식 주문
+   */
   async food() {
     return await Console.readLineAsync(select_menu);
   },
--- src/controller/ChristmasController.js
+++ src/controller/ChristmasController.js
@@ -6,18 +6,47 @@ import OutputView from "../OutputView.js";
 import preDiscountAmount from "../domain/preDiscountAmount.js";
 import Discount from "../domain/Discount.js";
 import totalDiscount from "../domain/totalDiscount.js";
-import badge from "../domain/badge.js"
+import badge from "../domain/badge.js";
 
+/**
+ * 크리스마스 할인 이벤트 로직 제어하는 컨트롤러 클래스
+ * @class
+ */
 class ChristmasController {
+  /**
+   * 할인 객체
+   * @type {Discount}
+   * @private
+   */
   #discount = new Discount();
 
+  /**
+   * ChristmasController 인스턴스 생성
+   * @constructor
+   */
   constructor() {
+    /**
+     * 애플리케이션 시작 메시지 출력
+     */
     OutputView.start();
   }
 
+  /**
+   * 사용자로부터 날짜를 입력 받는 메서드
+   * @async
+   * @returns {Promise<string>} 입력받은 날짜
+   */
   async dateInput() {
     return this.#handleDateInput();
   }
+
+  /**
+   * 날짜 입력 처리를 담당하는 메서드
+   * @async
+   * @private
+   * @returns {Promise<string>} 입력받은 날짜
+   * @throws {InputError} 입력이 유효하지 않은 경우 발생하는 예외
+   */
   async #handleDateInput() {
     try {
       const date = await InputView.date();
@@ -29,9 +58,22 @@ class ChristmasController {
     }
   }
 
+  /**
+   * 사용자로부터 음식을 입력받는 메서드
+   * @async
+   * @returns {Promise<string>} 입력받은 음식
+   */
   async foodInput() {
     return this.#handleFoodInput();
   }
+
+  /**
+   * 음식 입력 처리를 담당하는 메서드
+   * @async
+   * @private
+   * @returns {Promise<string>} 입력받은 음식
+   * @throws {InputError} 입력이 유효하지 않은 경우 발생하는 예외
+   */
   async #handleFoodInput() {
     try {
       const food = await InputView.food();
@@ -43,6 +85,11 @@ class ChristmasController {
     }
   }
 
+  /**
+   * 크리스마스 이벤트 로직을 처리하는 메서드
+   * @param {string} date - 날짜
+   * @param {string} food - 음식 주문
+   */
   handleChristmasLogic(date, food) {
     OutputView.preView(date);
     const menu = orderMenu(food);
